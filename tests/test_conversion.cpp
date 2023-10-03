79153892a6d09841010a595fe3e1853b35077240
axwhyzee <siah_weehung@yahoo.com>
2023-10-03 17:28:31 +0800

    change vec to str

--- tests/test_conversion.cpp
+++ tests/test_conversion.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include "../loadData.h"
 
 
@@ -8,7 +9,7 @@
  * @param tgt   Array of 0s and 1s that the array of bytes is meant to be equal to
  * @return Whether bytes matches tgt
  */
-bool _cmp_bytes(unsigned char *bytes, vector<bool> tgt)
+bool _cmp_bytes(unsigned char *bytes, string tgt)
 {
     int ib=0, it=0;
     unsigned char byte;
@@ -21,7 +22,7 @@ bool _cmp_bytes(unsigned char *bytes, vector<bool> tgt)
         for (int _ = 0; _ < 8; _++) {
             bit = 128 & byte;
             byte <<= 1;
-            if (tgt[it++] != bit) return false;
+            if (tgt[it++]-'0' != bit) return false;
         }
     }
     return true;
@@ -34,30 +35,30 @@ bool _cmp_bytes(unsigned char *bytes, vector<bool> tgt)
 void test_int_to_bytes()
 {
     int v;
-    vector<bool> bits;
+    string bits;
     unsigned char* bytes;
 
     // Test 1.1
     v = 0;
-    bits = {0,0,0,0,0,0,0,0};
+    bits = "00000000";
     bytes = intToBytes(v, (int) 1);
     cout << (_cmp_bytes(bytes, bits) ? "[SUCCESS]" : "[FAILED]") << " Test 1.1: Convert " << v << " to bytes " << endl;
 
     // Test 1.2
     v = 1;
-    bits = {0,0,0,0,0,0,0,1};
+    bits = "00000001";
     bytes = intToBytes(v, (int) 1);
     cout << (_cmp_bytes(bytes, bits) ? "[SUCCESS]" : "[FAILED]") << " Test 1.2: Convert " << v << " to bytes " << endl;
 
     // Test 1.3
     v = 130;
-    bits = {1,0,0,0,0,0,1,0};
+    bits = "10000010";
     bytes = intToBytes(v, (int) 1);
     cout << (_cmp_bytes(bytes, bits) ? "[SUCCESS]" : "[FAILED]") << " Test 1.3: Convert " << v << " to bytes " << endl;
 
     // Test 1.4
     v = 255;
-    bits = {1,1,1,1,1,1,1,1};
+    bits = "11111111";
     bytes = intToBytes(v, (int) 1);
     cout << (_cmp_bytes(bytes, bits) ? "[SUCCESS]" : "[FAILED]") << " Test 1.4: Convert " << v << " to bytes " << endl;
 }
