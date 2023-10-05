72294aeaa74f7567379350f2f3b02ef55b4f4f2b
axwhyzee <siah_weehung@yahoo.com>
2023-10-05 16:45:59 +0800

    complete node helper functions for borrow and merge

--- Node/node.cpp
+++ Node/node.cpp
@@ -1,23 +1,81 @@
+#include <string.h>
 #include "node.h"
+#include "../loadData.h"
 
 
-void InternalNode :: merge(InternalNode* right, InternalNode* parent) {
+// ---------------- InternalNode ----------------
+void InternalNode :: merge(InternalNode* right) {
     Node** _children = right->children;
     float* _keys = right->keys;
+    int i;
     
-    for (int i=0; i<right->numKeysInserted; i++) {
+    for (i=0; i<right->numKeysInserted; i++) {
         keys[numKeysInserted+i] = _keys[i];
-        children[numKeysInserted+i+1] = _children[i] ? _children[i] : _children[i+1];
+        children[numKeysInserted+i+1] = _children[i];
     }
+    children[numKeysInserted+i+1] = _children[i];
 }    
 
 
-void LeafNode :: merge(LeafNode* right, InternalNode* parent) {
+void InternalNode :: borrowLeft(InternalNode* left) {
+    // shift all data down by 1 slot
+    std::memcpy(children+1, children, 1);
+    std::memcpy(keys, keys+1, 1);
+
+    // copy over borrowed data from left sibling
+    children[0] = left->children[left->numKeysInserted];
+    (left->numKeysInserted)--;
+    keys[0] = children[1]->keys[0];
+}
+
+
+void InternalNode :: borrowRight(InternalNode* right) {
+    // copy over borrowed data from right sibling
+    keys[numKeysInserted] = right->children[0]->keys[0];
+    numKeysInserted++;
+    children[numKeysInserted] = right->children[0];
+    
+    // shift all data down by 1 slot (in sibling)
+    std::memcpy(right->children, right->children+1, right->numKeysInserted);
+    (right->numKeysInserted)--;
+    std::memcpy(right->keys, right->keys+1, right->numKeysInserted);
+}
+
+
+// ---------------- LeafNode ----------------
+void LeafNode :: merge(LeafNode* right) {
     Record** _records = right->records;
     float* _keys = right->keys;
+    int i;
     
-    for (int i=0; i<right->numKeysInserted; i++) {
+    for (i=0; i<right->numKeysInserted; i++) {
         keys[numKeysInserted+i] = _keys[i];
-        records[numKeysInserted+i+1] = _records[i] ? _records[i] : _records[i+1];
+        records[numKeysInserted+i+1] = _records[i];
     }
+    records[numKeysInserted+i+1] = _records[i];
+}
+
+
+void LeafNode :: borrowLeft(LeafNode* left) {
+    // shift all data down by 1 slot
+    std::memcpy(records+1, records, numKeysInserted+1);
+    std::memcpy(keys+1, keys, numKeysInserted);
+
+    // copy over borrowed data from left sibling
+    (left->numKeysInserted)--;
+    records[0] = left->records[left->numKeysInserted];
+    keys[0] = left->keys[left->numKeysInserted];
+}
+
+
+void LeafNode :: borrowRight(LeafNode* right) {
+    // copy over borrowed data from right sibling
+    records[numKeysInserted] = right->records[0];
+    keys[numKeysInserted] = right->keys[0];
+    numKeysInserted++;
+    
+    // shift all data down by 1 slot (in sibling)
+    right->numKeysInserted--;
+    std::memcpy(right->records, right->records+1, right->numKeysInserted);
+    std::memcpy(right->keys, right->keys+1, right->numKeysInserted);
 }
--- Node/node.h
+++ Node/node.h
@@ -20,11 +20,27 @@ typedef struct InternalNode: Node {
 
     /**
      * @brief Merge current node with a sibling internal node on the right
+     *        Merging is done right after deletion & shifting of key + ptr
      * 
      * @param right Sibling internal node on the right of this node
-     * @param parent Parent internal node of this and the sibling
     */
-    void merge(InternalNode* right, InternalNode* parent);    
+    void merge(InternalNode* right);    
+
+
+    /**
+     * @brief Borrow last pointer & key from sibling internal node on the left
+     * 
+     * @param left Sibling internal node on the left of this node
+    */
+    void borrowLeft(InternalNode* left);
+
+
+    /**
+     * @brief Borrow first pointer & key from sibling internal node on the right
+     * 
+     * @param left Sibling internal node on the right of this node; 
+    */
+    void borrowRight(InternalNode* right)
 };
 
 
@@ -38,9 +54,25 @@ typedef struct LeafNode: Node {
 
     /**
      * @brief Merge current node with a sibling leaf node on the right
+     *        Merging is done right after deletion & shifting of key + ptr
      * 
      * @param right Sibling leaf node on the right of this node
-     * @param parent Parent internal node of this and the sibling
     */
-    void merge(LeafNode* right, InternalNode* parent);
+    void merge(LeafNode* right);
+
+
+    /**
+     * @brief Borrow last pointer & key from sibling leaf node on the left
+     * 
+     * @param left Sibling leaf node on the left of this node; 
+    */
+    void borrowLeft(LeafNode* left);
+
+
+    /**
+     * @brief Borrow first pointer & key from sibling leaf node on the right
+     * 
+     * @param left Sibling leaf node on the right of this node; 
+    */
+    void borrowRight(LeafNode* right)
 };
