5237a21e051b95ea54d1362777816414062f4aa3
axwhyzee <siah_weehung@yahoo.com>
2023-10-06 15:54:02 +0800

    Update node.h

--- Node/node.h
+++ Node/node.h
@@ -1,76 +1,45 @@
+#ifndef NODE_H
+#define NODE_H
 #include "../Block Manager/blockManager.h"
 
 
 #define NUM_KEYS 39
+#define MIN_LEAF_KEYS (NUM_KEYS+1)/2
+#define MIN_INTERNAL_KEYS NUM_KEYS/2
 
 
-typedef struct Node {
+struct Node {
     // Given the size of the key and the size of a pointer, the number of keys/pointers should be 39
     float keys[NUM_KEYS];
     int numKeysInserted;
+
+    virtual ~Node() {};
 };
 
 
-typedef struct InternalNode: Node {
+struct InternalNode: Node {
     Node* children[NUM_KEYS+1];
-    int numChildrenNodes;
-
-    // struct InternalNode* parent = NULL;
 
     /**
-     * @brief Merge current node with a sibling internal node on the right
-     *        Merging is done right after deletion & shifting of key + ptr
-     * 
-     * @param right Sibling internal node on the right of this node
-    */
-    void merge(InternalNode* right);    
-
-
-    /**
-     * @brief Borrow last pointer & key from sibling internal node on the left
+     * @brief Merge current node with a sibling internal node on the left
      * 
      * @param left Sibling internal node on the left of this node
     */
-    void borrowLeft(InternalNode* left);
+    void mergeLeft(InternalNode* left);    
 
 
     /**
-     * @brief Borrow first pointer & key from sibling internal node on the right
+     * @brief Merge current node with a sibling internal node on the right
      * 
-     * @param left Sibling internal node on the right of this node; 
+     * @param right Sibling internal node on the right of this node
     */
-    void borrowRight(InternalNode* right);
+    void mergeRight(InternalNode* right);    
 };
 
 
-typedef struct LeafNode: Node {
+struct LeafNode: Node {
     Record* records[NUM_KEYS];
-    // int numRecordsInserted;
     LeafNode* next;
-
-    // InternalNode* parent = NULL;
-
-    /**
-     * @brief Merge current node with a sibling leaf node on the right
-     *        Merging is done right after deletion & shifting of key + ptr
-     * 
-     * @param right Sibling leaf node on the right of this node
-    */
-    void merge(LeafNode* right);
-
-
-    /**
-     * @brief Borrow last pointer & key from sibling leaf node on the left
-     * 
-     * @param left Sibling leaf node on the left of this node; 
-    */
-    void borrowLeft(LeafNode* left);
-
-
-    /**
-     * @brief Borrow first pointer & key from sibling leaf node on the right
-     * 
-     * @param left Sibling leaf node on the right of this node; 
-    */
-    void borrowRight(LeafNode* right);
 };
+
+#endif
