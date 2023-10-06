296b722314d233630e025910b97f3057c78acce6
axwhyzee <siah_weehung@yahoo.com>
2023-10-06 16:09:50 +0800

    deletion tests

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
 #include "tests/test_conversion.cpp"
+#include "tests/test_deletion.cpp"
 
 using namespace std;
 
 
 int main() {
     test_conversion();
+    test_deletion();
     
     return 0;
 }
